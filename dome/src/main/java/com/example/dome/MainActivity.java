61aadc8f5124d4bb841a93c82efeb42c2e9ba1fc
AlbertChang <cb123456>
2018-06-08 11:48:45 +0800

    Initial commit

---
+++ dome/src/main/java/com/example/dome/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.dome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * ================================================================
+ * 创建时间：2018-6-8 11:45:16
+ * 创 建 人：Mr.常
+ * 文件描述：
+ * 码    农：你不是我记忆中Bug、但致命的程度没两样！
+ * ================================================================
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
