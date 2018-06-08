ffebbd92be54c752cf5648c7e48bf94b947181ed
AlbertChang <cb123456>
2018-06-08 16:25:35 +0800

    更新v1.0.3

--- dome/src/main/java/com/example/dome/MainActivity.java
+++ dome/src/main/java/com/example/dome/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.dome;
 
+import android.Manifest;
+import android.app.Application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ViewUtils;
+
+import com.jess.arms.utils.DialogUtils;
+import com.jess.arms.utils.PermissionUtil;
+import com.jess.arms.widget.dialog.SweetAlertDialog;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import me.jessyan.rxerrorhandler.core.RxErrorHandler;
+import timber.log.Timber;
 
 /**
  * ================================================================
@@ -12,10 +23,52 @@ import android.os.Bundle;
  * ================================================================
  */
 public class MainActivity extends AppCompatActivity {
-
+    public SweetAlertDialog mDialog;//对话框
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        PermissionUtil.requestPermission(new PermissionUtil.RequestPermission() {
+                                             @Override
+                                             public void onRequestPermissionSuccess() {
+                                                 showDialog("提示", "授权成功", SweetAlertDialog.SUCCESS_TYPE);
+                                             }
+
+                                             @Override
+                                             public void onRequestPermissionFailure() {
+                                                 showDialog("提示", "您有未授予的权限，可能导致部分功能闪退，请点击\"设置\"授权相关权限", SweetAlertDialog.ERROR_TYPE);
+                                             }
+                                         }, new RxPermissions(this), getRxErrorHandler(getApplication()),
+                /**
+                 * 注释后面有 1 的为显示确认访问、无 1 的为隐藏默认访问
+                 */
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.WAKE_LOCK,
+                Manifest.permission.WRITE_SETTINGS);
+    }
+
+
+
+    public void showDialog(String title, String content, int dialogType) {
+    mDialog=DialogUtils.getInstance().getDialog(this,title,content,dialogType,false, new SweetAlertDialog.OnSweetClickListener() {
+        @Override
+        public void onClick(SweetAlertDialog sweetAlertDialog) {
+            mDialog.dismiss();
+        }
+    });
+        mDialog.show();
+    }
+
+    public RxErrorHandler getRxErrorHandler(Application mApplication){
+        return   RxErrorHandler
+                .builder()
+                .with(mApplication)
+                .responseErrorListener((context, t) -> Timber.e("异常"))
+                .build();
     }
 }
